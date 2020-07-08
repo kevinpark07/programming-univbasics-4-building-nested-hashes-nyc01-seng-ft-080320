76123f3b54276cc5518f081f47a25769e85e2db5
Kevin Park <kevin.park07@yahoo.com>
2020-07-08 21:54:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
       },
       :names => {
         :reading_railroad => {},
-        :pennsylvania_railraod =>{},
+        :pennsylvania_railraod => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
       }

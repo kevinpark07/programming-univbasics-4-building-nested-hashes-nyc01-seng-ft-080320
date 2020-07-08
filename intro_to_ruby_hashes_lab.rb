17ae2be49fc763eaf0e009ff8541b277cb21b7a0
Kevin Park <kevin.park07@yahoo.com>
2020-07-08 21:46:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,6 +4,10 @@ end
 
 def monopoly_with_second_tier
   bash_hash = {:railroads => {}}
+  bash_hash [:pieces] = "4"
+  bash_hash [:rent_in_dollars] = {}
+  bash_hash [:names] = {}
+  puts bash_hash
 
 end
 

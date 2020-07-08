a26f9a26bb818c2162256b6bc87d8d7345302e49
Kevin Park <kevin.park07@yahoo.com>
2020-07-08 21:59:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -41,11 +41,11 @@ def monopoly_with_fourth_tier
       },
       :names => {
         :reading_railroad => {mortgage_value: 100},
-        :pennsylvania_railroad => {mortgage value: 200},
+        :pennsylvania_railroad => {mortgage_value: 200},
         :b_and_o_railroad => {mortgage_value: 400},
         :shortline_railroad => {mortgage_value: 800}
       }
-    }
+}
 }
 
 end
